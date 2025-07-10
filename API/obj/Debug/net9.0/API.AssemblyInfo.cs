0cda8bb16915765f4c9f4dcb938f82b1d20d1789
Robert Gleason <l2ob122@gmail.com>
2025-07-09 20:04:55 -0700

    add Entity Framework Core packages and update project configuration

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2592a056f441ce0fa8170b5b7730feaad64e8409")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4df293fb772b2a387ae1e88cbed42d30637b693d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
