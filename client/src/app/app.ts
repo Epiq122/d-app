ad41780b12896c4b9d827602e2929e9e0dccb869
Robert Gleason <l2ob122@gmail.com>
2025-07-11 21:50:38 -0700

    display members list in app component and update HTTP request handling

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,12 @@ import { HttpClient } from '@angular/common/http';
 })
 export class App implements OnInit {
   private http = inject(HttpClient);
-
   protected readonly title = 'Dating App';
+  protected members = signal<any>([]);
 
   ngOnInit(): void {
     this.http.get('https://localhost:5001/api/members').subscribe({
-      next: (response) => console.log(response),
+      next: (response) => this.members.set(response),
       error: (error) => console.log(error),
       complete: () => console.log('Completed the http request'),
     });
